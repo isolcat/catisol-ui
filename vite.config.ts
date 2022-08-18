ec93a014a3310a71c0ed4a7b0cce1fd636cb950f
“isolcat” <308750414@qq.com>
2022-08-18 22:02:06 +0800

    组件库初始化

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
 
-    plugins: [vue()],
+    plugins: [
+        // 添加JSX插件
+        vueJsx({
+
+        }),
+        vue()],
 
 });
