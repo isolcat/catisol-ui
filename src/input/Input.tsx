3e41678c0bdfd3f8125bbdd319cbe1df3bdb1bac
“isolcat” <308750414@qq.com>
2022-12-02 10:55:03 +0800

    fix input

--- src/input/Input.tsx
+++ src/input/Input.tsx
@@ -5,7 +5,7 @@ export default defineComponent({
     name:"CInput",
     setup() {
         return () => (
-            <div class= "op80 text-lg fw300 m1 " >
+            <div class= "op80 text-lg fw300 m1" >
                 <input type= "text" placeholder="Please Input" class="b-rd-1" />
             </div>
         )
