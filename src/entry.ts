c9051fbf5908d13fc90e57508bc5a64de74e0544
“isolcat” <308750414@qq.com>
2023-01-01 13:41:21 +0800

    Introducing the link component

--- src/entry.ts
+++ src/entry.ts
@@ -1,11 +1,12 @@
 import { App } from "vue";
 import MyButton from "./button/Button"
 import Input from "./input";
+import Link from "./link";
 import SFCButton from "./SFCButton.vue";
 import JSXButton from "./JSXButton";
 
 // 导出单独组件
-export { MyButton, SFCButton, JSXButton, Input }
+export { MyButton, SFCButton, JSXButton, Input, Link }
 
 // 编写一个插件，实现一个install方法
 
@@ -15,5 +16,6 @@ export default {
         app.component(SFCButton.name, SFCButton)
         app.component(JSXButton.name, JSXButton)
         app.component(Input.name, Input)
+        app.component(Link.name, Link)
     }
 }
